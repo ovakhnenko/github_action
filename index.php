68ca950ce19b7318ef85f6ad346b12fe79684d77
Aleksey Vakhnenko <gmail@vakhnenko.com>
2024-07-31 19:32:01 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World</p>'; ?>
+        <?php echo '<p>version 1.01</p>'; ?>
+    </body>
+</html>
